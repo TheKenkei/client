07272d232f1ad5db7db5f4c60a5af84483cb9364
TheKenkei <nowikow.witaliy@gmail.com>
2024-04-24 23:39:32 +0300

    feat: add dynamic title with `react-handle`

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,16 +1,19 @@
 import { AppStore } from './app';
 import { LinksStore } from './links';
 import { OrganizatonsStore } from './organizations';
+import { TitleStore } from './title';
 
 export type RootStore = {
   appStore: AppStore;
   orgsStore: OrganizatonsStore;
+  titleStore: TitleStore;
   linksStore: LinksStore;
 };
 
 const rootStore: RootStore = {
   appStore: new AppStore(),
   orgsStore: new OrganizatonsStore(),
+  titleStore: new TitleStore(),
   linksStore: new LinksStore(),
 };
 
