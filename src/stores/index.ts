dc334c8ea2e07bf9ec51409340bfb625ab9e753d
TheKenkei <nowikow.witaliy@gmail.com>
2024-04-27 22:44:44 +0300

    feat: add `MapsStore`
    the store that stores the selected card

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,14 +1,18 @@
+import { MapsStore } from '@app/stores/maps';
+
 import { AppStore } from './app';
 import { OrganizatonsStore } from './organizations';
 
 export type RootStore = {
   appStore: AppStore;
   orgsStore: OrganizatonsStore;
+  mapsStore: MapsStore;
 };
 
 const rootStore: RootStore = {
   appStore: new AppStore(),
   orgsStore: new OrganizatonsStore(),
+  mapsStore: new MapsStore(),
 };
 
 export default rootStore;
